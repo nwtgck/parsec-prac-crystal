1347dbd53ff91e70ccd6c54ce58333e21077c190
Ryo Ota <nwtgck@gmail.com>
2018-01-11 06:44:23 +0900

    [add] Add an example using mdo

--- main.cr
+++ main.cr
@@ -1,7 +1,17 @@
 require "parsec"
+require "crz"
 
 parser = Parsec.char('c')
 
 p(parser)
 p(parser.parse("c"))
 p(parser.parse("d"))
+
+parser = mdo({
+  _         <= Parsec.string("hello"),
+  num_arr   <= Parsec.many_1(Parsec.digit),
+  Parsec::Parser.of(num_arr.map{|e| e.to_i})
+})
+
+p(parser.parse("hello54613"))
+# => [5, 4, 6, 1, 3]
