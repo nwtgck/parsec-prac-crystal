40bdc554d0cafbb1d18efd791291ad3d60a4dbc7
Ryo Ota <nwtgck@gmail.com>
2018-01-11 06:48:46 +0900

    [refactor] Add type notations

--- main.cr
+++ main.cr
@@ -1,17 +1,24 @@
 require "parsec"
 require "crz"
 
-parser = Parsec.char('c')
+def use_char()
+    parser : Parsec::Parser(Char) = Parsec.char('c')
+    p(parser)
+    p(parser.parse("c"))
+    p(parser.parse("d"))
+end
 
-p(parser)
-p(parser.parse("c"))
-p(parser.parse("d"))
 
-parser = mdo({
-  _         <= Parsec.string("hello"),
-  num_arr   <= Parsec.many_1(Parsec.digit),
-  Parsec::Parser.of(num_arr.map{|e| e.to_i})
-})
+def use_mdo()
+    parser : Parsec::Parser(Array(Int32)) = mdo({
+        _         <= Parsec.string("hello"),
+        num_arr   <= Parsec.many_1(Parsec.digit),
+        Parsec::Parser.of(num_arr.map{|e| e.to_i})
+    })
 
-p(parser.parse("hello54613"))
-# => [5, 4, 6, 1, 3]
+    p(parser.parse("hello54613"))
+    # => [5, 4, 6, 1, 3]
+end
+
+use_char()
+use_mdo()
